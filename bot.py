26903988d08026b9d214247a36d25ecacb58f1a7
leenuu <61344683+leenuu@users.noreply.github.com>
2021-01-31 11:07:48 +0900

    base

---
+++ bot.py
@@ -0,0 +1,98 @@
+import discord 
+from main import bank
+from discord.ext import commands 
+
+app = commands.Bot(command_prefix='>') 
+bot = bank()
+
+#####################################################################################################
+@app.event 
+async def on_ready(): 
+    print(app.user.name, 'has connected to Discord!') 
+    await app.change_presence(status=discord.Status.online, activity=None) 
+    print("ready")
+
+#####################################################################################################
+
+@app.command() 
+@commands.has_role("은행원")
+async def 관리(t, *, name_money): 
+    name = name_money.split(' ')[0]
+    money = int(name_money.split(' ')[1])
+    if bot.manage(name, money) == 1:
+        await t.send('존제하지 않는 유저입니다.') 
+
+    if bot.manage(name, money) == 0:
+        await t.send(f'{name} 님의 돈을 {money}만큼 추가했습니다.') 
+
+@관리.error
+async def 관리_error(t, err):
+    if isinstance(err, commands.MissingRole):
+        await t.send('당신은 권한이 없습니다.') 
+    if isinstance(err, commands.CommandInvokeError):
+        await t.send('명령어를 다시 확인해주세요.') 
+
+
+#####################################################################################################
+
+@app.command() 
+@commands.has_any_role("은행 고객원", "은행원")
+async def 만들기(t, *, name): 
+    if bot.add_user(name) == 1:
+        await  t.send(f'{name} 님 이미 계좌가 있습니다.')
+    else:
+        await t.send(f'{name} 님의 계좌를 추가했습니다.') 
+
+#####################################################################################################
+
+@app.command() 
+@commands.has_role("은행원")
+async def 저장(t): 
+    bot.save()
+    await t.send('저장완료')
+
+#####################################################################################################
+
+@app.command() 
+@commands.has_any_role("은행 고객원", "은행원")
+async def 확인(t, *, name): 
+    await t.send(f'{name}님의 현재 돈은 {bot.cheack(name)}')
+
+@확인.error
+async def 확인_err(t, err):
+    if isinstance(err,commands.CommandInvokeError):
+        await t.send('존제하지 않는 유저입니다.') 
+
+#####################################################################################################
+
+# @app.command() 
+# @commands.has_role("일반 유저")
+# async def on_message(message):
+# 	for i in range(len(message.guild.roles)):
+# 		print(message.guild.roles[i].id)
+
+#####################################################################################################
+
+@app.command() 
+@commands.has_any_role("은행원" , "은행 고객원")
+async def 도움(t):
+    embed = discord.Embed(title="이누 분신", description="디스코드 화폐봇 입니다.", color=0x62c1cc)
+    embed.add_field(name=">만들기 (유저맨션) ", value="맨션한 유저의 화폐를 할당받는 계좌를 만듬니다.", inline=False)
+    embed.add_field(name=">확인 (유저맨션)", value="맨션한 유저의 현제 소지하는 화폐을 확인합니다.", inline=False)
+    await t.send(embed=embed)
+
+#####################################################################################################
+
+@app.command() 
+@commands.has_role("은행원")
+async def 관리도움(t):
+    embed = discord.Embed(title="이누 분신", description="디스코드 화폐봇 입니다.", color=0x62c1cc)
+    embed.add_field(name=">관리 (유저맨션) (추가 또는 빼고싶은 만큼의 돈)", value="맨션한 유저의 돈을 더하거나 뺌니다.", inline=False)
+    embed.add_field(name=">저장", value="정보를 저장합니다.", inline=False)
+    await t.send(embed=embed)
+
+#####################################################################################################
+
+
+app.run('ODA1MDI4MjMzOTIxNDI5NTc0.YBU69w.DH7Lx-x7u1FUEhxerCeLFNfz2lA')
+
---
+++ main.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import openpyxl
+
+class bank:
+    
+    def __init__(self):
+        print("초기 설정 완료")
+        self.data = dict()
+        self.bank_data = pd.Series([self.data.keys(),self.data.values()], index = ['name', 'money'])
+
+        try:    
+            self.files = openpyxl.load_workbook('data.xlsx')
+            self.xlsx = self.files.active
+            self.load()
+
+        except FileNotFoundError:
+            files = openpyxl.Workbook()
+            files.save('data.xlsx')            
+            
+            self.files = openpyxl.load_workbook('data.xlsx')
+            self.xlsx = self.files.active
+            
+    def add_user(self, user):
+        if user in self.data.keys():
+            print("이미 존제하는 유저입니다.")
+            return 1
+
+        else:
+            print(f"{user} 추가 성공")
+            self.data[user] = 0
+            return 0
+
+    def manage(self, user, money):
+        if user in self.data.keys():
+            print(f"{user} 한테서 {money} 추가했습니다.")
+            self.data[user] += money
+            return 0
+        
+        else:
+            print('존제하지 않는 유저입니다.')
+            return 1
+
+    def save(self):
+        for i in range(len(self.data)):
+            self.xlsx.cell(row=i+1, column=1).value = list(self.data.keys())[i]
+            self.xlsx.cell(row=i+1, column=2).value = self.data[list(self.data.keys())[i]]
+            
+        self.files.save('data.xlsx')
+
+    def load(self):
+        num = 1
+        while True:
+            if self.xlsx.cell(row=num, column=1).value != None:
+                self.data[self.xlsx.cell(row=num, column=1).value] = self.xlsx.cell(row=num, column=2).value
+                num += 1
+
+            else:
+                break
+            
+        self.bank_data = pd.Series([self.data.keys(),self.data.values()], index = ['name', 'money'])
+    
+    def cheack(self, user):
+        return self.data[user]
+
+# bot = bank()
+# bot.add_user('야스')
+# bot.add_user('잭스')
+# print(bot.bank_data)
+# bot.manage('이누', 100)
+# print(bot.bank_data)
+# bot.save()
+
+
+
+
+
